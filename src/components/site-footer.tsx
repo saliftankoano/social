4d547a0fe185cdd53b86e3dc9640891821331b70
Salif Tankoano <tanksalif@gmail.com>
2025-02-15 15:27:08 -0500

    update footer

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -1,4 +1,5 @@
 import { DiscordLogoIcon, TwitterLogoIcon } from "@radix-ui/react-icons";
+import Image from "next/image";
 import Link from "next/link";
 
 const footerNavs = [
@@ -70,31 +71,25 @@ export function SiteFooter() {
   return (
     <footer>
       <div className="mx-auto w-full max-w-screen-xl xl:pb-2">
-        <div className="md:flex md:justify-between px-8 p-4 py-16 sm:pb-16 gap-4">
-          <div className="mb-12 flex-col flex gap-4">
+        <div className="gap-4 p-4 px-8 py-16 sm:pb-16 md:flex md:justify-between">
+          <div className="mb-12 flex flex-col gap-4">
             <Link href="/" className="flex items-center gap-2">
-              <img
-                src="https://magicui.design/icon.png"
-                className="h-8 w-8 text-primary"
-              />
-              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-                Magic UI
-              </span>
+              <Image src="/logo.png" alt="VirallIQ" width={80} height={15} />
             </Link>
-            <p className="max-w-xs">UI Library for Design Engineers</p>
+            <p className="max-w-xs">AI-powered social media content creation</p>
           </div>
-          <div className="grid grid-cols-1 gap-8 sm:gap-10 sm:grid-cols-3">
+          <div className="grid grid-cols-1 gap-8 sm:grid-cols-3 sm:gap-10">
             {footerNavs.map((nav) => (
               <div key={nav.label}>
-                <h2 className="mb-6 text-sm tracking-tighter font-medium text-gray-900 uppercase dark:text-white">
+                <h2 className="mb-6 text-sm font-medium uppercase tracking-tighter text-gray-900 dark:text-white">
                   {nav.label}
                 </h2>
-                <ul className="gap-2 grid">
+                <ul className="grid gap-2">
                   {nav.items.map((item) => (
                     <li key={item.name}>
                       <Link
                         href={item.href}
-                        className="cursor-pointer text-gray-400 hover:text-gray-200 duration-200 font-[450] text-sm"
+                        className="cursor-pointer text-sm font-[450] text-gray-400 duration-200 hover:text-gray-200"
                       >
                         {item.name}
                       </Link>
@@ -106,23 +101,23 @@ export function SiteFooter() {
           </div>
         </div>
 
-        <div className="flex flex-col sm:flex-row sm:flex sm:items-center sm:justify-between rounded-md border-neutral-700/20 py-4 px-8 gap-2">
-          <div className="flex space-x-5 sm:justify-center sm:mt-0">
+        <div className="flex flex-col gap-2 rounded-md border-neutral-700/20 px-8 py-4 sm:flex sm:flex-row sm:items-center sm:justify-between">
+          <div className="flex space-x-5 sm:mt-0 sm:justify-center">
             {footerSocials.map((social) => (
               <Link
                 key={social.name}
                 href={social.href}
-                className="text-gray-500 hover:text-gray-900 dark:hover:text-gray-600 fill-gray-500 hover:fill-gray-900 dark:hover:fill-gray-600"
+                className="fill-gray-500 text-gray-500 hover:fill-gray-900 hover:text-gray-900 dark:hover:fill-gray-600 dark:hover:text-gray-600"
               >
                 {social.icon}
                 <span className="sr-only">{social.name}</span>
               </Link>
             ))}
           </div>
-          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          <span className="text-sm text-gray-500 dark:text-gray-400 sm:text-center">
             Copyright © {new Date().getFullYear()}{" "}
             <Link href="/" className="cursor-pointer">
-              Magic UI
+              VirallIQ
             </Link>
             . All Rights Reserved.
           </span>
